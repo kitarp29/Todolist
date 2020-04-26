6702ea16809f1f4ce7b34a46e7e500632bc843dd
Pratik Singh <56778773+kitarp29@users.noreply.github.com>
2020-04-26 23:45:43 +0530

    v1

---
+++ app.js
@@ -0,0 +1,31 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+
+const app=express();
+var ans=["Eat😋","Sleep😴","Code💻","Repeat♻"];
+app.set('view engine','ejs');
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.static("public"));
+app.get("/",function(req,res){
+let today= new Date();
+
+let options={
+  weekday:"long",
+  day:"numeric",
+  month:"long"
+};
+let day=today.toLocaleDateString("hi-IN",options);
+
+res.render("list",{pk:day,lily:ans});
+});
+
+app.post("/",function(req,res){
+let anws = req.body.lily;
+ans.push(anws);
+res.redirect("/");
+  // console.log(ans);
+});
+
+app.listen(3000,function(){
+  console.log("I'm running BITCH!!");
+});

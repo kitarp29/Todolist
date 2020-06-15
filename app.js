69fb6ad8bfd4b71802cef567651745890944d5e3
Pratik Singh <56778773+kitarp29@users.noreply.github.com>
2020-06-16 01:44:58 +0530

    Database linked

--- app.js
+++ app.js
@@ -1,11 +1,53 @@
 const express = require("express");
 const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
 
 const app=express();
-var ans=["Eat😋","Sleep😴","Code💻","Repeat♻"];
+var ans=[];
 app.set('view engine','ejs');
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
+
+mongoose.connect("mongodb+srv://Pratik:Pratik2902@cluster0-io8ki.mongodb.net/todolistDB?retryWrites=true&w=majority/todolistDB",{ useUnifiedTopology: true ,useNewUrlParser: true});
+
+const item ={
+  name:{
+    type:String,
+    required:[true]
+  }
+};
+
+const Item = mongoose.model("Item",item);
+
+const item1= new Item({
+  name:"Eat😋"
+});
+const item2= new Item({
+  name:"Sleep😴"
+});
+
+const item3= new Item({
+  name:"Code💻"
+});
+const item4= new Item({
+  name:"Repeat♻"
+});
+
+
+const defaultitems =[item1,item2,item3,item4];
+
+// Item.insertMany(defaultitems,function(err){
+//   if(err)
+//   {
+//     console.log(err);
+//   }
+//   else{
+//     console.log("yipeeee!!! DB bnn gya h");
+//   }
+// });
+
+
+
 app.get("/",function(req,res){
 let today= new Date();
 
@@ -16,14 +58,60 @@ let options={
 };
 let day=today.toLocaleDateString("hi-IN",options);
 
-res.render("list",{pk:day,lily:ans});
+
+Item.find({},(err,results)=>{
+  if(results.length ===0)
+  {
+    Item.insertMany(defaultitems,function(err){
+      if(err)
+      {
+        console.log(err);
+      }
+    });
+    // console.log(err);
+    res.redirect("/");
+  }
+  else if(err)
+  {
+    console.log(err);
+  }
+  else{
+    res.render("list",{pk:day,lily:results});
+    // console.log(results);
+  }
+})
+
 });
 
+
+
 app.post("/",function(req,res){
-let anws = req.body.lily;
-ans.push(anws);
+const itemName = req.body.lily;
+
+const item =new Item({
+  name:itemName
+});
+
+item.save();
+
 res.redirect("/");
   // console.log(ans);
+});
+
+app.post("/delete",function(req,res){
+  // console.log(req.body.checkbox);
+  const checkeditem=req.body.checkbox;
+  Item.findByIdAndRemove(checkeditem,function(err)
+{
+  if(err){
+    console.log(err);
+  }
+  else{
+    res.redirect("/");
+    // console.log("YIPEE-KI-YAY MOTHERFUCCCKER!!");
+  }
+});
+
 });
 
 app.listen(process.env.PORT || 3000,function(){
